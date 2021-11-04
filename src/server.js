2ce8fd39868a1a7bff6e9f765cef5ce2fbf7b1fe
Dennis Tai <swotai@gmail.com>
2021-11-04 13:36:44 -0700

    Move the base level to /links
    # Conflicts:
    #	src/server.js

--- src/server.js
+++ src/server.js
@@ -14,8 +14,8 @@ const cssLinksFromAssets = (assets, entrypoint) => {
   return assets[entrypoint]
     ? assets[entrypoint].css
       ? assets[entrypoint].css
-          .map(asset => `<link rel="stylesheet" href="${asset}">`)
-          .join('')
+        .map(asset => `<link rel="stylesheet" href="${asset}">`)
+        .join('')
       : ''
     : '';
 };
@@ -24,8 +24,8 @@ const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
   return assets[entrypoint]
     ? assets[entrypoint].js
       ? assets[entrypoint].js
-          .map(asset => `<script src="${asset}"${extra}></script>`)
-          .join('')
+        .map(asset => `<script src="${asset}"${extra}></script>`)
+        .join('')
       : ''
     : '';
 };
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === 'production') {
 server
   .use(helmet.hidePoweredBy())
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/', (req, res) => {
+  .get('/links', (req, res) => {
     const context = {};
     const markup = renderToString(
       <StaticRouter context={context} location={req.url}>
@@ -79,78 +79,64 @@ server
         <meta name="description" content="${runtimeConfig.META_DESCRIPTION}">
         <meta name="author" content="${runtimeConfig.META_AUTHOR}">
         <meta name="keywords" content="${runtimeConfig.META_KEYWORDS}">
-        <meta name="robots" content="${
-          runtimeConfig.META_INDEX_STATUS || 'noindex'
+        <meta name="robots" content="${runtimeConfig.META_INDEX_STATUS || 'noindex'
         }">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          runtimeConfig.OG_SITE_NAME
-            ? `<meta property="og:site_name" content="${runtimeConfig.OG_SITE_NAME}" />`
-            : ''
+        ${runtimeConfig.OG_SITE_NAME
+          ? `<meta property="og:site_name" content="${runtimeConfig.OG_SITE_NAME}" />`
+          : ''
         }
-        ${
-          runtimeConfig.OG_TITLE
-            ? `<meta property="og:title" content="${runtimeConfig.OG_TITLE}" />`
-            : ''
+        ${runtimeConfig.OG_TITLE
+          ? `<meta property="og:title" content="${runtimeConfig.OG_TITLE}" />`
+          : ''
         }
-        ${
-          runtimeConfig.OG_DESCRIPTION
-            ? `<meta property="og:description" content="${runtimeConfig.OG_DESCRIPTION}" />`
-            : ''
+        ${runtimeConfig.OG_DESCRIPTION
+          ? `<meta property="og:description" content="${runtimeConfig.OG_DESCRIPTION}" />`
+          : ''
         }
-        ${
-          runtimeConfig.OG_URL
-            ? `<meta property="og:url" content="${runtimeConfig.OG_URL}" />`
-            : ''
+        ${runtimeConfig.OG_URL
+          ? `<meta property="og:url" content="${runtimeConfig.OG_URL}" />`
+          : ''
         }
         <meta property="og:type" content="siteweb" />
-        ${
-          runtimeConfig.OG_IMAGE
-            ? `
+        ${runtimeConfig.OG_IMAGE
+          ? `
                 <meta property="og:image" content="${runtimeConfig.OG_IMAGE}" />
                 <meta property="og:image:secure_url" content="${runtimeConfig.OG_IMAGE}" />
             `
-            : ''
+          : ''
         }
-        ${
-          runtimeConfig.OG_IMAGE_WIDTH
-            ? `<meta property="og:image:width" content="${runtimeConfig.OG_IMAGE_WIDTH}" />`
-            : ''
+        ${runtimeConfig.OG_IMAGE_WIDTH
+          ? `<meta property="og:image:width" content="${runtimeConfig.OG_IMAGE_WIDTH}" />`
+          : ''
         }
-        ${
-          runtimeConfig.OG_IMAGE_HEIGHT
-            ? `<meta property="og:image:height" content="${runtimeConfig.OG_IMAGE_HEIGHT}" />`
-            : ''
+        ${runtimeConfig.OG_IMAGE_HEIGHT
+          ? `<meta property="og:image:height" content="${runtimeConfig.OG_IMAGE_HEIGHT}" />`
+          : ''
         }
-        ${
-          runtimeConfig.TWITTER_TITLE
-            ? `<meta property="twitter:title" content="${runtimeConfig.TWITTER_TITLE}" />`
-            : ''
+        ${runtimeConfig.TWITTER_TITLE
+          ? `<meta property="twitter:title" content="${runtimeConfig.TWITTER_TITLE}" />`
+          : ''
         }
-        ${
-          runtimeConfig.TWITTER_DESCRIPTION
-            ? `<meta property="twitter:description" content="${runtimeConfig.TWITTER_DESCRIPTION}" />`
-            : ''
+        ${runtimeConfig.TWITTER_DESCRIPTION
+          ? `<meta property="twitter:description" content="${runtimeConfig.TWITTER_DESCRIPTION}" />`
+          : ''
         }
-        ${
-          runtimeConfig.TWITTER_CARD
-            ? `<meta property="twitter:card" content="${runtimeConfig.TWITTER_CARD}" />`
-            : ''
+        ${runtimeConfig.TWITTER_CARD
+          ? `<meta property="twitter:card" content="${runtimeConfig.TWITTER_CARD}" />`
+          : ''
         }
-        ${
-          runtimeConfig.TWITTER_IMAGE
-            ? `<meta property="twitter:image" content="${runtimeConfig.TWITTER_IMAGE}" />`
-            : ''
+        ${runtimeConfig.TWITTER_IMAGE
+          ? `<meta property="twitter:image" content="${runtimeConfig.TWITTER_IMAGE}" />`
+          : ''
         }
-        ${
-          runtimeConfig.TWITTER_SITE
-            ? `<meta property="twitter:site" content="${runtimeConfig.TWITTER_SITE}" />`
-            : ''
+        ${runtimeConfig.TWITTER_SITE
+          ? `<meta property="twitter:site" content="${runtimeConfig.TWITTER_SITE}" />`
+          : ''
         }
-        ${
-          runtimeConfig.TWITTER_CREATOR
-            ? `<meta property="twitter:creator" content="${runtimeConfig.TWITTER_CREATOR}" />`
-            : ''
+        ${runtimeConfig.TWITTER_CREATOR
+          ? `<meta property="twitter:creator" content="${runtimeConfig.TWITTER_CREATOR}" />`
+          : ''
         }
         <link href="css/fonts.css" rel="stylesheet">
         <link rel="stylesheet" href="css/normalize.css">
@@ -158,9 +144,8 @@ server
         <link rel="stylesheet" href="css/brands.css">
         ${cssLinksFromAssets(assets, 'client')}
         <link rel="icon" type="image/png" href="${runtimeConfig.FAVICON_URL}">
-        ${
-          runtimeConfig.GA_TRACKING_ID
-            ? `
+        ${runtimeConfig.GA_TRACKING_ID
+          ? `
             <!-- Global site tag (gtag.js) - Google Analytics -->
             <script async src="https://www.googletagmanager.com/gtag/js?id=${runtimeConfig.GA_TRACKING_ID}"></script>
             <script>
@@ -170,19 +155,17 @@ server
 
         gtag('config', '${runtimeConfig.GA_TRACKING_ID}');
       </script>`
-            : ''
+          : ''
         }
-        ${
-          runtimeConfig.UMAMI_WEBSITE_ID && runtimeConfig.UMAMI_APP_URL
-            ? `
+        ${runtimeConfig.UMAMI_WEBSITE_ID && runtimeConfig.UMAMI_APP_URL
+          ? `
             <!-- Umami Analytics -->
             <script async defer data-website-id="${runtimeConfig.UMAMI_WEBSITE_ID}" src="${runtimeConfig.UMAMI_APP_URL}/umami.js">
             </script>`
-            : ''
+          : ''
         }
-        ${
-          runtimeConfig.MATOMO_URL && runtimeConfig.MATOMO_SITE_ID
-            ? `
+        ${runtimeConfig.MATOMO_URL && runtimeConfig.MATOMO_SITE_ID
+          ? `
             <!-- Matomo -->
             <script type="text/javascript">
                 var _paq = window._paq || [];
@@ -202,18 +185,17 @@ server
                 })();
             </script>
             <!-- Matomo End -->`
-            : ''
+          : ''
         }
 
     </head>
     <body>
-        ${
-          runtimeConfig.MATOMO_URL && runtimeConfig.MATOMO_SITE_ID
-            ? `
+        ${runtimeConfig.MATOMO_URL && runtimeConfig.MATOMO_SITE_ID
+          ? `
             <!-- Matomo Image Tracker-->
             <img referrerpolicy="no-referrer-when-downgrade" src="${runtimeConfig.MATOMO_URL}/matomo.php?idsite=${runtimeConfig.MATOMO_SITE_ID}&amp;rec=1" style="border:0" alt="" />
             <!-- End Matomo -->`
-            : ''
+          : ''
         }
         <div id="root">${markup}</div>
         <script>window.env = ${serialize(runtimeConfig)};</script>
